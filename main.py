1b4c45ebca8d65315d7967b1f3334205fc081a60
Carlos Rosado <crosadoro16@gmail.com>
2025-06-05 23:48:32 +0200

    Nuevas modificaciones: - Se ha añadido la barra de seleccion de idiomas, la cual esta totalmente funcional - Se han añadido nuevos idiomas. - Se ha añadido la funcion de poder acceder al boton de dispach. - Añadido el boton home, para poder volver a la pantalla principal.
    Proximos updates:
    - Poder mostrar la pantalla de dispach, en el cual tendremos un menu de radio, una radio interna, un mapa y apartado de mensaje de textos.
    - Mostrar pantalla de busqueda de ciudadanos.
    - Buena renderizacion de la aplicación.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from traducciones import traducciones
 class TPolicia:
     def __init__(self, page: ft.Page):
         self.page = page
-        self.lang = page.client_locale[:2] #Detecta el idioma segun el sistema
+        self.lang = "en" #Detecta el idioma segun el sistema
         self.t = traducciones.get(self.lang, traducciones["en"]) #Traduccion inicial
         self.language_dropdown = None
         self.content_area = None
@@ -28,11 +28,21 @@ class TPolicia:
             on_change= self.change_language
         )
 
-        def create_button(icon: str, label: str) -> ft.Container:
+        self.content_area = ft.Column()
+        self.page.add(self.language_dropdown, self.content_area)
+        self.build_dashboard()
+
+    def change_language(self, e):
+        
+        self.lang = e.control.value
+        self.t = traducciones.get(self.lang, traducciones["en"])
+        self.build_dashboard()
+
+    def create_button(icon: str, label_key: str) -> ft.Container:
             return ft.Container(
                 content=ft.Column([
                     ft.Icon(name=icon, size=40, color="white"),
-                    ft.Text(label, color="white", size=16, weight="bold")
+                    ft.Text(self.t[label_key], color="white", size=16, weight="bold")
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER),
@@ -42,31 +52,41 @@ class TPolicia:
                 border_radius=10,
                 alignment=ft.alignment.center,
                 ink=True,
-                on_click=lambda e: print(f"{label} clicked"),
+                on_click=lambda e: print(f"{self.t[label_key]} clicked"),
             )
+    
+    def build_dashboard(self):
+        #Limpia el contenido actual
+        self.content_area.controls.clear()
 
         buttons = [
-            create_button("gamepad", "DISPATCH"),
-            create_button("fingerprint", "CITIZEN SEARCH"),
-            create_button("description", "REPORTS"),
-            create_button("directions_car", "VEHICLES"),
-            create_button("gavel", "CRIMINAL CODE"),
-            create_button("search", "SEARCH AND CAPTURE"),
-            create_button("account_balance_wallet", "DEBTORS"),
-            create_button("gavel", "FEDERAL MANAGEMENT"),
-            create_button("badge", "AGENT MANAGEMENT"),
-            create_button("videocam", "SECURITY CAMERAS"),
+            self.create_button("gamepad", "DISPATCH"),
+            self.create_button("fingerprint", "CITIZEN SEARCH"),
+            self.create_button("description", "REPORTS"),
+            self.create_button("directions_car", "VEHICLES"),
+            self.create_button("gavel", "CRIMINAL CODE"),
+            self.create_button("search", "SEARCH AND CAPTURE"),
+            self.create_button("account_balance_wallet", "DEBTORS"),
+            self.create_button("gavel", "FEDERAL MANAGEMENT"),
+            self.create_button("badge", "AGENT MANAGEMENT"),
+            self.create_button("videocam", "SECURITY CAMERAS"),
         ]
 
-        page.add(
-            ft.Text("POLICE ON DUTY", size=24, color="white"),
-            ft.GridView(
-                runs_count=4,
+        self.content_area.controls.append(
+            ft.Text(self.t["on_duty"], size=24, color="white")
+        )
+        
+        self.content_area.controls.append(
+             ft.GridView(
+                  runs_count=4,
                 max_extent=200,
                 child_aspect_ratio=1.3,
                 spacing=15,
+                run_spacing=15,
                 controls=buttons
             )
         )
+
+        self.page.update()
 
 ft.app(target=TPolicia)
